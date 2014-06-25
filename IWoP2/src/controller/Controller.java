d543589d4b3c2a7d9ebff60ebc925c56f104edc8
Stefan Okruhlica <okruhste@fel.cvut.cz>
2014-06-25 17:11:42 +0200

    Controller a View

---
+++ IWoP2/src/controller/Controller.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import data.Game;
+import gui.View;
+
+/**
+ *
+ * @author Stefan
+ */
+public class Controller {
+    
+    Game game;
+    View view;
+
+    public Controller(Game game, View view) {
+        this.game = game;
+        this.view = view;
+    }
+    
+    
+    
+}
---
+++ IWoP2/src/gui/View.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import data.Game;
+
+/**
+ *
+ * @author Stefan
+ */
+public class View {
+    
+    Game game;
+
+    public View(Game game) {
+        this.game = game;
+    }
+    
+    
+    
+}
