1820253c5b46330f80f48669b00dde84ad4de9b0
Stefan Okruhlica <okruhste@fel.cvut.cz>
2014-06-26 10:45:16 +0200

    story

--- IWoP2/src/data/Story.java
+++ IWoP2/src/data/Story.java
@@ -4,10 +4,36 @@
  */
 package data;
 
+import data.interfaces.IQuest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Stefan
  */
+
+
 public class Story {
+    
+    private Map<World, Map<Integer, List<IQuest>>> quests;
+
+    public Story(World[] worlds) {
+        quests = new HashMap<>();
+        for (World world : worlds) {
+            quests.put(world, new HashMap<Integer, List<IQuest>>());
+            quests.get(world).put(0, new ArrayList<IQuest>());
+            quests.get(world).put(1, new ArrayList<IQuest>());
+            quests.get(world).put(2, new ArrayList<IQuest>());
+            quests.get(world).put(3, new ArrayList<IQuest>());
+        }
+    }
+    
+    public List<IQuest> getActiveMainQuests(World world){
+        return quests.get(world).get(0);
+    }
+
     
 }
