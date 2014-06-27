93a788b5ba1498c8687822e66ebc92b44a70f652
Stefan Okruhlica <okruhste@fel.cvut.cz>
2014-06-27 18:01:01 +0200

    NextTurnButtonListener

---
+++ IWoP2/src/controller/MainScreenListeners/NextTurnButtonListener.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.MainScreenListeners;
+
+import data.Game;
+import gui.View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Stefan
+ */
+public class NextTurnButtonListener implements ActionListener{
+    
+    Game game;
+    View view;
+
+    public NextTurnButtonListener(Game game, View view) {
+        this.game = game;
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        game.nextTurn();
+        view.createMainScreen(game.getActivePlayer().getLocation(), game.getActivePlayer().getX(), game.getActivePlayer().getY());
+    }
+    
+}
