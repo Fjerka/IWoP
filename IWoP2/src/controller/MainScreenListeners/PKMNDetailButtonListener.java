9c8c3e7d00ebc06fda8afa82c38471cd2b305274
Stefan Okruhlica <okruhste@fel.cvut.cz>
2014-06-27 18:25:43 +0200

    new listener

---
+++ IWoP2/src/controller/MainScreenListeners/PKMNDetailButtonListener.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.MainScreenListeners;
+
+import data.Game;
+import gui.View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Stefan
+ */
+public class PKMNDetailButtonListener implements ActionListener{
+    
+    int index;
+    Game game;
+    View view;
+
+    public PKMNDetailButtonListener(int index, Game game, View view) {
+        this.index = index;
+        this.game = game;
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+}
