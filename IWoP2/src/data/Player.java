ad7f09d92ebf1bacfc0deb55729d47fb0a7a8465
Stefan Okruhlica <okruhste@fel.cvut.cz>
2014-06-27 15:34:14 +0200

    uprava statu playera

--- IWoP2/src/data/Player.java
+++ IWoP2/src/data/Player.java
@@ -15,9 +15,10 @@ import java.util.Map;
  */
 public class Player extends ATrainer {
 
-    private int currentExp, maxExp, currentHunger, maxHunger,
-            currentMovementPoints, maxMovementPoints, currentExhaustion,
+    private int currentExp, maxExp,  maxHunger,
+            currentMovementPoints, maxMovementPoints, usedMovementPoints, currentExhaustion,
             maxExhaustion, currentCapacity, maxCapacity;
+    private double currentHunger;
     private List<ITrainingMethod> trainingMethods;
     private List<IPokemon> storedPokemons;
     private Map<World, List<IBadge>> badges;
@@ -27,7 +28,7 @@ public class Player extends ATrainer {
     private int x, y;
 
     public Player(String firstName, String surname,int level, boolean sex,  int currentExp, int maxExp,
-            int currentHunger, int maxHunger, int currentMovementPoints,int maxMovementPoints,
+            double currentHunger, int maxHunger, int currentMovementPoints,int maxMovementPoints,
             int currentExhaustion, int maxExhaustion, int currentCapacity, int maxCapacity,
             List<ITrainingMethod> trainingMethods, List<IPokemon> storedPokemons,
             Map<World, List<IBadge>> badges, Story story, World world, Location location, int x, int y,
@@ -58,7 +59,10 @@ public class Player extends ATrainer {
         if (x > 0 && location.getMap()[x-1][y].isAccessible(story, world) && 
                 location.getMap()[x - 1][y].getMovementPoints() <= currentMovementPoints) {
             currentMovementPoints -= location.getMap()[x - 1][y].getMovementPoints();
+            usedMovementPoints += location.getMap()[x - 1][y].getMovementPoints();
+            currentHunger -= location.getMap()[x][y - 1].getMovementPoints()/2.0;
             x--;
+            checkExhaustion();
         }
     }
 
@@ -66,7 +70,10 @@ public class Player extends ATrainer {
         if (x < location.getMap().length - 1 && location.getMap()[x + 1][y].isAccessible(story, world) && 
                 location.getMap()[x + 1][y].getMovementPoints() <= currentMovementPoints) {
             currentMovementPoints -= location.getMap()[x + 1][y].getMovementPoints();
+            usedMovementPoints += location.getMap()[x + 1][y].getMovementPoints();
+            currentHunger -= location.getMap()[x][y - 1].getMovementPoints()/2.0;
             x++;
+            checkExhaustion();
         }
     }
 
@@ -74,7 +81,10 @@ public class Player extends ATrainer {
         if (y > 0 && location.getMap()[x][y - 1].isAccessible(story, world) &&
                 location.getMap()[x][y - 1].getMovementPoints() <= currentMovementPoints) {
             currentMovementPoints -= location.getMap()[x][y - 1].getMovementPoints();
+            usedMovementPoints += location.getMap()[x][y - 1].getMovementPoints();
+            currentHunger -= location.getMap()[x][y - 1].getMovementPoints()/2.0;
             y--;
+            checkExhaustion();
         }
     }
 
@@ -82,7 +92,17 @@ public class Player extends ATrainer {
         if (y < location.getMap()[x].length - 1 && location.getMap()[x][y + 1].isAccessible(story, world) && 
                 location.getMap()[x][y + 1].getMovementPoints() <= currentMovementPoints) {
             currentMovementPoints -= location.getMap()[x][y + 1].getMovementPoints();
+            usedMovementPoints += location.getMap()[x][y + 1].getMovementPoints();
+            currentHunger -= location.getMap()[x][y - 1].getMovementPoints()/2.0;
             y++;
+            checkExhaustion();
+        }
+    }
+    
+    private void checkExhaustion(){
+        if (usedMovementPoints >= 10){
+            usedMovementPoints -= 10;
+            currentExhaustion--;
         }
     }
 
@@ -98,7 +118,7 @@ public class Player extends ATrainer {
         return y;
     }
 
-    public int getCurrentHunger() {
+    public double getCurrentHunger() {
         return currentHunger;
     }
 
